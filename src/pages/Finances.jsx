aeb6363ddbb13ff8aa8f8d4eb09b8a02bce7f67c
Hazel Belmont <hparkerbelmont@gmail.com>
2026-04-19 19:19:09 -0400

    Update link href for FICO page

--- src/pages/Finances.jsx
+++ src/pages/Finances.jsx
@@ -16,7 +16,7 @@ export default function Finances() {
         <div className="finances-page ">
             <MobileTitleBar pageTitle="Finances" backButton />
             <div className="d-flex flex-column justify-content-center align-items-center mt-4">
-                <Button href={`${baseURL}/finances/fico`} variant="outline-secondary" className="align-items-center justify-content-center d-flex flex-column" style={{ borderRadius: "50vh", height: "25vh", width: "25vh", border: "2px var(--secondary) solid" }}>
+                <Button href={`${baseURL}finances/fico`} variant="outline-secondary" className="align-items-center justify-content-center d-flex flex-column" style={{ borderRadius: "50vh", height: "25vh", width: "25vh", border: "2px var(--secondary) solid" }}>
                 <h4 className="text-center " style={{ fontSize: "2vh", paddingInline: "2vw" }}>Your FICO Score:</h4>
                 <h2 className="p-0" style={{ fontSize: "7vh"}}>760</h2>
                 </Button>
