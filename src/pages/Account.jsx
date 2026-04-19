11740f24975c3708ac18672fdc57fc552da99f00
Hazel Belmont <hparkerbelmont@gmail.com>
2026-04-19 09:47:36 -0400

    Add page titles to each page

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,6 +1,12 @@
+import { useEffect } from "react";
 import "../styles/account.css";
 
 export default function Account() {
+    /* Change the page title on page load */
+    useEffect(() => {
+        document.title = "Edit Account Info";
+    }, []);
+
     return (
         <div className="account-page">
             {/* replace this line with all of your code! */}
--- src/pages/FICO.jsx
+++ src/pages/FICO.jsx
@@ -1,6 +1,12 @@
+import { useEffect } from "react";
 import "../styles/fico.css";
 
 export default function FICO() {
+    /* Change the page title on page load */
+    useEffect(() => {
+        document.title = "FICO Score Detail";
+    }, []);
+
     return (
         <div className="fico-page">
             {/* replace this line with all of your code! */}
