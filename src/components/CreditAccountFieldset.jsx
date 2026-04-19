c2851a772036dc006b2fa7ae0b86ad8c86084baa
Hazel Belmont <hparkerbelmont@gmail.com>
2026-04-19 01:52:35 -0400

    Add form validation and saving account info to local storage

--- src/components/CreditAccountFieldset.jsx
+++ src/components/CreditAccountFieldset.jsx
@@ -1,7 +1,9 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import {Col, Form, InputGroup, Row} from "react-bootstrap";
 
-export default function CreditAccountFieldset({accountInfo, setAccountInfo, index, loan = false}) {    
+export default function CreditAccountFieldset({accountInfo, setAccountInfo, index}) {
+    const [isLoan, setLoan] = useState((accountInfo.loan === "true") || false);
+
     function handleInputChange(event) {
         const {name, value} = event.target;
         setAccountInfo((allAccountInfo) => {
@@ -20,20 +22,20 @@ export default function CreditAccountFieldset({accountInfo, setAccountInfo, inde
                 <Col xs={6}>
                     <Form.Label>Account Type</Form.Label>
                 </Col>
-                <InputGroup as={Col}>
-                    <Form.Select value={accountInfo.loan} onChange={handleInputChange} name="loan">
-                        <option value={false}>Credit Card</option>
-                        <option value={true}>Loan</option>
+                <Col>
+                    <Form.Select value={accountInfo.loan === "true"} onChange={(event) => {setLoan(event.target.value === "true"); handleInputChange(event);}} name="loan">
+                        <option value="false">Credit Card</option>
+                        <option value="true">Loan</option>
                     </Form.Select>
-                </InputGroup>
+                </Col>
             </Form.Group>
             <Form.Group as={Row} className="mb-3 d-flex align-items-center">
                 <Col xs={6}>
-                    <Form.Label>{loan ? "Principal" : "Credit Limit"}</Form.Label>
+                    <Form.Label>{accountInfo.loan === "true" ? "Principal" : "Credit Limit"}</Form.Label>
                 </Col>
                 <InputGroup as={Col}>
                     <InputGroup.Text>$</InputGroup.Text>
-                    <Form.Control value={accountInfo.limit} type="number" placeholder="0.00" onChange={handleInputChange} name="limit" />
+                    <Form.Control value={accountInfo.limit} type="number" placeholder="0.00" min={0} onChange={handleInputChange} name="limit" />
                 </InputGroup>
             </Form.Group>
             <Form.Group as={Row} className="mb-3 d-flex align-items-center">
@@ -42,7 +44,7 @@ export default function CreditAccountFieldset({accountInfo, setAccountInfo, inde
                 </Col>
                 <InputGroup as={Col}>
                     <InputGroup.Text>$</InputGroup.Text>
-                    <Form.Control value={accountInfo.balance} type="number" placeholder="0.00" onChange={handleInputChange} name="balance" />
+                    <Form.Control value={accountInfo.balance} type="number" placeholder="0.00" min={0} max={isLoan ? "" : accountInfo.limit} onChange={handleInputChange} name="balance" />
                 </InputGroup>
             </Form.Group>
             <Form.Group as={Row} className="mb-3 d-flex align-items-center">
@@ -50,7 +52,7 @@ export default function CreditAccountFieldset({accountInfo, setAccountInfo, inde
                     <Form.Label>APR</Form.Label>
                 </Col>
                 <InputGroup as={Col}>
-                    <Form.Control value={accountInfo.apr} type="number" placeholder="0.00" onChange={handleInputChange} name="apr" />
+                    <Form.Control value={accountInfo.apr} type="number" placeholder="0.00" min={0} onChange={handleInputChange} name="apr" />
                     <InputGroup.Text>%</InputGroup.Text>
                 </InputGroup>
             </Form.Group>
@@ -59,8 +61,8 @@ export default function CreditAccountFieldset({accountInfo, setAccountInfo, inde
                     <Form.Label>Account Age</Form.Label>
                 </Col>
                 <InputGroup as={Col}>
-                    <Form.Control value={accountInfo.age} type="number" placeholder="0.00" onChange={handleInputChange} name="age" />
-                    <InputGroup.Text>Months</InputGroup.Text>
+                    <Form.Control value={accountInfo.age} type="number" placeholder="0.00" min={0} onChange={handleInputChange} name="age" />
+                    <InputGroup.Text>{accountInfo.age === 1 ? "Month" : "Months"}</InputGroup.Text>
                 </InputGroup>
             </Form.Group>
         </fieldset>
--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,19 +1,26 @@
-import { useState } from "react";
-import { Button, Container, Form } from "react-bootstrap";
+import { useRef, useState } from "react";
+import { Button, Container, Form, Row } from "react-bootstrap";
 import CreditAccountFieldset from "../components/CreditAccountFieldset";
 import "../styles/account.css";
-import { PlusCircle } from "react-bootstrap-icons";
+import { PlusCircle, Save } from "react-bootstrap-icons";
 import MobileTitleBar from "../components/MobileTitleBar";
 
 export default function Account() {
-    const [accountInfo, setAccountInfo] = useState([{
-        accountName: "Credit Account",
-        limit: 0,
-        balance: 0,
-        apr: 0,
-        age: 0,
-        loan: false
-    }]);
+    const formRef = useRef(null);
+
+    const [accountInfo, setAccountInfo] = useState(() => {
+        const savedAccountInfo = localStorage.getItem("accountInfo");
+        return savedAccountInfo !== null
+            ? JSON.parse(savedAccountInfo)
+            : [{
+                accountName: "Credit Account",
+                limit: 0,
+                balance: 0,
+                apr: 0,
+                age: 0,
+                loan: "false"
+            }];
+    });
 
     function handleAddFieldset() {
         setAccountInfo([...accountInfo, {
@@ -22,8 +29,14 @@ export default function Account() {
             balance: 0,
             apr: 0,
             age: 0,
-            loan: false
+            loan: "false"
         }]);
+    }
+
+    function handleSave() {
+        if(formRef.current.checkValidity()) {
+            localStorage.setItem('accountInfo', JSON.stringify(accountInfo));
+        }
     }
 
     console.log(accountInfo)
@@ -32,10 +45,16 @@ export default function Account() {
         <div className="account-page">
             <MobileTitleBar pageTitle="Edit Account Information" />
             <Container fluid>
-                <Form>
+                <Form ref={formRef} validated>
                     {accountInfo.map((info, index) => <CreditAccountFieldset accountInfo={info} allAccountInfo={accountInfo} setAccountInfo={setAccountInfo} index={index} key={index} />)}
+                    <Form.Control.Feedback type="invalid">Please fix any errors before saving your preferences.</Form.Control.Feedback>
                 </Form>
-                <Button onClick={handleAddFieldset} className="bg-secondary border-none d-flex align-items-center"><PlusCircle className="me-2" />Add Account</Button>
+                <Container fluid>
+                    <Row className="d-flex justify-content-between">
+                        <Button onClick={handleAddFieldset} variant="secondary" className="d-flex flex-grow-0 flex-shrink-1 align-items-center col-auto"><PlusCircle className="me-2" />Add Account</Button>
+                        <Button onClick={handleSave} variant="info" className="d-flex flex-grow-0 flex-shrink-1 align-items-center col-auto"><Save className="me-2" />Save</Button>
+                    </Row>
+                </Container>
             </Container>
         </div>
     );
