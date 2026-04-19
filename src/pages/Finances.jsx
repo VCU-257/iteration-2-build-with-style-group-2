d7d17907e0941c030e616ea5a478029641333399
Hazel Belmont <hparkerbelmont@gmail.com>
2026-04-19 19:17:30 -0400

    Update link href for FICO page

--- src/pages/Finances.jsx
+++ src/pages/Finances.jsx
@@ -1,11 +1,12 @@
 import { useEffect } from "react";
-import "../styles/finances.css";
 import MobileTitleBar from "../components/MobileTitleBar";
 import FinancesCards from "../components/FinancesCards";
-import { Button, Container, Form, Row } from "react-bootstrap";
-import { href } from "react-router";
+import { Button } from "react-bootstrap";
+import "../styles/finances.css";
 
 export default function Finances() {
+    const baseURL = import.meta.env.BASE_URL.includes("#") ? import.meta.env.BASE_URL : `${import.meta.env.BASE_URL}#/`;
+
     /* Change the page title on page load */
     useEffect(() => {
         document.title = "Finances";
@@ -13,9 +14,9 @@ export default function Finances() {
 
     return (
         <div className="finances-page ">
-            <MobileTitleBar pageTitle="Finances" backButtonHref={"/"} />
+            <MobileTitleBar pageTitle="Finances" backButton />
             <div className="d-flex flex-column justify-content-center align-items-center mt-4">
-                <Button href="/fico" variant="outline-secondary" className="align-items-center justify-content-center d-flex flex-column" style={{ borderRadius: "50vh", height: "25vh", width: "25vh", border: "2px var(--secondary) solid" }}>
+                <Button href={`${baseURL}/finances/fico`} variant="outline-secondary" className="align-items-center justify-content-center d-flex flex-column" style={{ borderRadius: "50vh", height: "25vh", width: "25vh", border: "2px var(--secondary) solid" }}>
                 <h4 className="text-center " style={{ fontSize: "2vh", paddingInline: "2vw" }}>Your FICO Score:</h4>
                 <h2 className="p-0" style={{ fontSize: "7vh"}}>760</h2>
                 </Button>
