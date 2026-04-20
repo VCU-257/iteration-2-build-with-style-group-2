03738ea696bebf19e4ef53949aa544f25e092f99
Hazel Belmont <hparkerbelmont@gmail.com>
2026-04-19 20:11:26 -0400

    Add useFicoScore hook to show a calculated FICO score.

--- src/pages/Finances.jsx
+++ src/pages/Finances.jsx
@@ -3,6 +3,7 @@ import MobileTitleBar from "../components/MobileTitleBar";
 import FinancesCards from "../components/FinancesCards";
 import { Button } from "react-bootstrap";
 import "../styles/finances.css";
+import { useFicoScore } from "../hooks";
 
 export default function Finances() {
     const baseURL = import.meta.env.BASE_URL.includes("#") ? import.meta.env.BASE_URL : `${import.meta.env.BASE_URL}#/`;
@@ -12,13 +13,15 @@ export default function Finances() {
         document.title = "Finances";
     }, []);
 
+    const score = useFicoScore();
+
     return (
         <div className="finances-page ">
             <MobileTitleBar pageTitle="Finances" backButton />
             <div className="d-flex flex-column justify-content-center align-items-center mt-4">
                 <Button href={`${baseURL}finances/fico`} variant="outline-secondary" className="align-items-center justify-content-center d-flex flex-column" style={{ borderRadius: "50vh", height: "25vh", width: "25vh", border: "2px var(--secondary) solid" }}>
                 <h4 className="text-center " style={{ fontSize: "2vh", paddingInline: "2vw" }}>Your FICO Score:</h4>
-                <h2 className="p-0" style={{ fontSize: "7vh"}}>760</h2>
+                <h2 className="p-0" style={{ fontSize: "7vh"}}>{score}</h2>
                 </Button>
                 <FinancesCards /> 
             </div>
