af7c9c16dd167efb17322f6d385b1575cd1c02dd
Hazel Belmont <hparkerbelmont@gmail.com>
2026-04-19 20:05:19 -0400

    Create FICO score calculation hook and update account form with required fields.

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,9 +1,9 @@
 import { useEffect, useRef, useState } from "react";
-import { Button, Container, Form, Row } from "react-bootstrap";
-import CreditAccountFieldset from "../components/CreditAccountFieldset";
-import "../styles/account.css";
+import { Button, Col, Container, Form, Row } from "react-bootstrap";
 import { PlusCircle, Save } from "react-bootstrap-icons";
+import CreditAccountFieldset from "../components/CreditAccountFieldset";
 import MobileTitleBar from "../components/MobileTitleBar";
+import "../styles/account.css";
 
 export default function Account() {
     const formRef = useRef(null);
@@ -18,9 +18,23 @@ export default function Account() {
                 balance: 0,
                 apr: 0,
                 age: 0,
-                loan: "false"
+                loan: "false",
+                missedPayments: 0
             }];
     });
+
+    const [additionalInfo, setAdditionalInfo] = useState(() => {const savedAdditionalInfo = localStorage.getItem("additionalInfo");
+        return savedAdditionalInfo !== null
+            ? JSON.parse(savedAdditionalInfo)
+            : {
+                inquiries: 0
+            };
+    });
+
+    function handleAdditionalInfoChange(event) {
+        const {name, value} = event.target;
+        setAdditionalInfo((additionalInfo) => {return {...additionalInfo, [name]: value};});
+    }
 
     function handleAddFieldset() {
         setAccountInfo([...accountInfo, {
@@ -29,17 +43,17 @@ export default function Account() {
             balance: 0,
             apr: 0,
             age: 0,
-            loan: "false"
+            loan: "false",
+            missedPayments: 0
         }]);
     }
 
     function handleSave() {
         if(formRef.current.checkValidity()) {
             localStorage.setItem('accountInfo', JSON.stringify(accountInfo));
+            localStorage.setItem('additionalInfo', JSON.stringify(additionalInfo));
         }
     }
-
-    console.log(accountInfo)
 
     /* Change the page title on page load */
     useEffect(() => {
@@ -52,6 +66,15 @@ export default function Account() {
             <Container fluid>
                 <Form ref={formRef} validated>
                     {accountInfo.map((info, index) => <CreditAccountFieldset accountInfo={info} allAccountInfo={accountInfo} setAccountInfo={setAccountInfo} index={index} key={index} />)}
+                    <h4>Additional Info</h4>
+                    <Form.Group as={Row} className="mb-3 d-flex align-items-center">
+                        <Col xs={6}>
+                            <Form.Label>Inquiries</Form.Label>
+                        </Col>
+                        <Col>
+                            <Form.Control value={additionalInfo.inquiries} type="number" placeholder="0" min={0} onChange={handleAdditionalInfoChange} name="inquiries" />
+                        </Col>
+                    </Form.Group>
                     <Form.Control.Feedback type="invalid">Please fix any errors before saving your preferences.</Form.Control.Feedback>
                 </Form>
                 <Container fluid>
